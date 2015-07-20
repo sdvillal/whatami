6df2be7224ff8ca216b126140c544065b01388d2
santi <sdvillal@gmail.com>
2015-07-20 14:40:57 +0200

    test that passes in both py2 and py3

--- whatami/tests/test_what.py
+++ whatami/tests/test_what.py
@@ -187,7 +187,11 @@ def test_configuration_nonids_prefix_postfix():
              non_id_keys=('verbose', 'n_jobs'),
              prefix_keys=('p3', 'p2'),
              postfix_keys=('p1', 'p2')).id()
-    assert str(excinfo.value) == 'Some identifiers ({\'p2\'}) appear in both first and last, they should not'
+    if PY3:
+        expected = 'Some identifiers ({\'p2\'}) appear in both first and last, they should not'
+    else:
+        expected = 'Some identifiers (set([u\'p2\'])) appear in both first and last, they should not'
+    assert str(excinfo.value) == expected
 
 
 def test_configuration_as_string():
