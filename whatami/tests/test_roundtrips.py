74d236f594b4724acab6cc9642e693e016765692
santi <sdvillal@gmail.com>
2015-07-30 17:24:30 +0200

    no covers

--- whatami/tests/test_roundtrips.py
+++ whatami/tests/test_roundtrips.py
@@ -15,7 +15,7 @@ def whatable2spectations(request):
     # Simple whatable
     if request.param == 'simple':
         @whatable
-        def rfc(X, y, n_trees=10, split=2, depth=None, criterion='gini', alpha=1.34):
+        def rfc(X, y, n_trees=10, split=2, depth=None, criterion='gini', alpha=1.34):  # pragma: no cover
             return X, y, n_trees, split, depth, criterion, alpha
         expected_id = "rfc(alpha=1.34,criterion='gini',depth=None,n_trees=10,split=2)"
         expected_what = What(name='rfc',
@@ -34,7 +34,7 @@ def whatable2spectations(request):
         a_tuple = (33, a_list, a_dict)
 
         @whatable
-        def rfc(X, y, t=a_tuple):
+        def rfc(X, y, t=a_tuple):    # pragma: no cover
             return X, y, t
         expected_id = "rfc(t=(33,['l',None,3.2],{'a':2,None:'c'}))"
         expected_what = What(name='rfc', conf={'t': a_tuple})
@@ -48,18 +48,18 @@ def whatable2spectations(request):
         a_tuple = (33, a_list, a_dict)
 
         @whatable
-        def rfc(X, y, t=a_tuple):
+        def rfc(X, y, t=a_tuple):  # pragma: no cover
             return X, y, t
 
         @whatable
-        def nests(nested=rfc):
+        def nests(nested=rfc):  # pragma: no cover
             return nested
         expected_id = "nests(nested=rfc(t=(33,['l',None,3.2],{'a':2,None:'c'})))"
         expected_what = What(name='nests',
                              conf={'nested': What(name='rfc', conf={'t': (33, ['l', None, 3.2], {None: 'c', 'a': 2})})})
         return nests, expected_id, expected_what
 
-    raise ValueError('Cannot find test case %s' % request.param)
+    raise ValueError('Cannot find test case %s' % request.param)    # pragma: no cover
 
 
 def test_roundtrip(whatable2spectations):
