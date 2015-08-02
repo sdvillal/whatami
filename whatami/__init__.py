5c8b5b31f1cdd083f50b39811e715219a4806cf2
santi <sdvillal@gmail.com>
2015-08-02 08:41:26 +0200

    refresh init

--- whatami/__init__.py
+++ whatami/__init__.py
@@ -1,6 +1,8 @@
 # coding=utf-8
 from __future__ import unicode_literals, absolute_import
+from .misc import *
 from .what import *
 from .parsers import *
+from .whatutils import *
 
-__version__ = '3.1.0'
+__version__ = '4.0.0-dev0'
