a7b95f882a4a02390753a9a8618789e4ead98224
santi <sdvillal@gmail.com>
2015-08-03 16:43:35 +0200

    very basic registry tests

--- whatami/registry.py
+++ whatami/registry.py
@@ -1,5 +1,10 @@
 # coding=utf-8
 """Simple implementations of central registries for whatami ids, nicknames, synonyms and contexts."""
+
+# Authors: Santi Villalba <sdvillal@gmail.com>
+# Licence: BSD 3 clause
+
+from __future__ import unicode_literals, absolute_import
 from whatami import what2id
 
 
@@ -12,43 +17,54 @@ class WhatamiRegistry(object):
         self._id2nick = {}
         self._nick2id = {}
 
-    def register(self, what, nickname, overwrite=False):
-        """Registers the nickname for the id of what."""
+    def register(self, what, nickname):
+        """Registers the nickname for the id of what; returns `what` in the interest of fluent interfaces."""
+
+        if what is None:
+            raise ValueError('what cannot be None')
+        if nickname is None:
+            raise ValueError('nickname cannot be None')
 
         whatid = what2id(what)
 
         i2n, n2i = self._id2nick, self._nick2id
 
-        if not overwrite:
-            # Ensure a one-to-one relationship; probably a nickname pointing to more than one id would be more useful
-            if nickname in n2i and not n2i[nickname] == whatid:
-                raise Exception('nickname "%s" is already associated with id "%s", delete it before updating' %
-                                (nickname, n2i[nickname]))
-            if whatid in i2n and not i2n[whatid] == nickname:
-                raise Exception('id "%s" is already associated with nickname "%s", delete it before updating' %
-                                (whatid, i2n[whatid][0]))
+        # Ensure a one-to-one relationship; probably a nickname pointing to more than one id would be more useful
+        if nickname in n2i and not n2i[nickname] == whatid:
+            raise Exception('nickname "%s" is already associated with id "%s", delete it before updating' %
+                            (nickname, n2i[nickname]))
+        if whatid in i2n and not i2n[whatid] == nickname:
+            raise Exception('id "%s" is already associated with nickname "%s", delete it before updating' %
+                            (whatid, i2n[whatid]))
 
         # Add binding
         i2n[whatid] = nickname
         n2i[nickname] = whatid
 
+        # Fluent
+        return what
+
     def list(self):
         """Returns a sorted list of tuples (nick, id)."""
         return sorted(self._nick2id.items())
 
-    def remove(self, nickname=None, whatid=None):
-        """Removes the entry corresponding to nickname or whatid.
+    def remove(self, nickname=None, what=None):
+        """Removes the entry corresponding to nickname or what.
         If both are provided, they must correspond to a single entry.
         """
+        whatid = None if what is None else what2id(what)
         if nickname is None and whatid is None:
-            return
+            raise ValueError('at least one of nickname or what must be provided')
         elif nickname is None:
             nickname = self.id2nick(whatid)
-        elif whatid is None:
+        elif what is None:
             whatid = self.nick2id(nickname)
         else:
-            assert self.id2nick(whatid) == nickname
-            assert self.nick2id(nickname) == whatid
+            old_whatid = self.nick2id(nickname)
+            old_nick = self.id2nick(nickname)
+            if old_whatid != whatid or old_nick != nickname:
+                raise ValueError('both whatid "%s"=="%s" and nickname "%s"=="%s" identities must hold' %
+                                 (whatid, old_whatid, nickname, old_nick))
         del self._nick2id[nickname]
         del self._id2nick[whatid]
 
@@ -63,7 +79,7 @@ class WhatamiRegistry(object):
     def nick_or_id(self, what):
         """Returns the nickname if it exists, otherwise it returns the id."""
         whatid = what2id(what)
-        return self._nick2id.get(whatid, whatid)
+        return self._id2nick.get(whatid, whatid)
 
     def reset(self):
         """Removes all entries in the registry."""
--- whatami/tests/test_registry.py
+++ whatami/tests/test_registry.py
@@ -1,108 +1,100 @@
 # coding=utf-8
 
-# ---- Fixtures and teardown
+# Authors: Santi Villalba <sdvillal@gmail.com>
+# Licence: BSD 3 clause
+
+from __future__ import unicode_literals, absolute_import
+import pytest
+from whatami import whatable
+
+from whatami.registry import WhatamiRegistry
 
 
-def teardown_function(_):
-    """After each run, wipe nicknames registry."""
-    What.reset_nicknames()
+@pytest.fixture()
+def registry():
+    return WhatamiRegistry()
 
 
-def test_whatable_nickname(c1):
-
-    class NicknamedConfigurable(object):
-        def what(self):
-            c = whatareyou(self)
-            c.nickname = 'bigforest'
-            return c
-
-    # nicknamed configurations
-    assert NicknamedConfigurable().what().nickname == 'bigforest'
-    assert NicknamedConfigurable().what().nickname_or_id() == 'bigforest'
-
-    # not nicknamed configurations
-    assert c1.what().nickname is None
-    assert c1.what().nickname_or_id() == "C1(length=1,p1='blah',p2='bleh')"
+WHAT2NICKS = ['rfc0']
 
 
-def test_regnick_with_what(c1):
-
-    # saving the what...
-    What.register_nickname('c1', c1, save_what=True)
-    assert c1.what().nickname == 'c1'
-    assert What.id2what(c1.what().id()) == c1
-    assert What.nickname2what('c1') == c1
-    assert What.id2nickname(c1.what().id()) == 'c1'
-    assert What.nickname2id('c1') == c1.what().id()
-    # not saving the what...
-    What.remove_nickname('c1')
-    What.register_nickname('c1', c1, save_what=False)
-    assert What.id2what(c1.what().id()) is None
-    assert What.nickname2what('c1') is None
-    # and of course not there if we haven't registered...
-    assert What.nickname2what('c2') is None
-    assert What.id2what('c2') is None
+@pytest.fixture(params=WHAT2NICKS, ids=WHAT2NICKS)
+def what2nick(request):
+    if request.param == 'rfc0':
+        @whatable
+        def rfc(x, y=3, z='xyz'):  # pragma: no cover
+            return x, y, z
+        whatid = "rfc(y=3,z='xyz')"
+        nick = 'rfc0'
+        return rfc, whatid, nick
+    raise ValueError('unknown fixture "%s"' % request.param)  # pragma: no cover
 
 
-def test_regnick_only_what():
+def test_register(registry, what2nick):
+
+    what, whatid, nick = what2nick
+
+    # register and access
+    with pytest.raises(ValueError):
+        registry.register(what, None)
+    with pytest.raises(ValueError):
+        registry.register(None, nick)
+    assert what == registry.register(what=what, nickname=nick)
+    assert nick == registry.nick_or_id(what)
+    assert whatid == registry.nick2id(nick)
+
+    # do not allow double registration
     with pytest.raises(Exception) as excinfo:
-        What.register_nickname('c1', 1)
-    expected = '"what" must be a whatable or a string, but is a <type \'int\'>' if not PY3 else \
-        '"what" must be a whatable or a string, but is a <class \'int\'>'
-    assert str(excinfo.value) == expected
+        registry.register(what, 'YOUSHALLNOTPASS')
+    assert 'id "%s" is already associated with nickname "%s", delete it before updating' % (whatid, nick) \
+           in str(excinfo.value)
 
-
-def test_regnick_remove_id(c1):
-    What.register_nickname('c1', c1, save_what=True)
-    # remove by id...
-    What.remove_id(c1.what().id())
-    # nothing in there
-    assert What.nickname2id('c1') is None
-    assert What.id2nickname(c1.what().id()) is None
-    assert What.id2what(c1.what().id()) is None
-
-
-def test_regnick_do_not_reregister(c1):
-    What.register_nickname('c1', c1)
-    # do not allow update...
-    What.register_nickname('c1', c1, save_what=False)
     with pytest.raises(Exception) as excinfo:
-        What.register_nickname('c1', 'blahblehblih')
-    assert str(excinfo.value) == 'nickname "c1" is already associated with id ' \
-                                 '"C1(length=1,p1=\'blah\',p2=\'bleh\')", delete it before updating'
-    with pytest.raises(Exception) as excinfo:
-        What.register_nickname('c2', c1.what().id())
-    assert str(excinfo.value) == 'id "%s" is already associated with nickname "c1", delete it before updating' %\
-                                 c1.what().id()
-    # no problem if we remove it first...
-    What.remove_nickname('c1')
-    assert c1.what().nickname is None
-    What.register_nickname('c1', c1)
-    assert c1.what().nickname == 'c1'
+        registry.register('YOUSHALLNOTPASS()', nick)
+    assert 'nickname "%s" is already associated with id "%s", delete it before updating' % (nick, whatid) \
+           in str(excinfo.value)
 
 
-def test_regnick_self(c1):
-    # self-registration
-    what = c1.what()
-    what.nickname = 'c1'
-    what.register_my_nickname()
-    what.nickname = None
-    assert what.nickname == 'c1'
-    assert c1.what().nickname == 'c1'  # Recall that a call to what() always return a new "What" object
-
-    # no problem on re-registering the same map
-    what.register_my_nickname()
-    assert what.nickname == 'c1'
-    assert c1.what().nickname == 'c1'
-    What.remove_nickname('c1')
-    assert what.nickname is None
-    assert c1.what().nickname is None
+def test_list(registry, what2nick):
+    what, whatid, nick = what2nick
+    registry.register(what=what, nickname=nick)
+    assert registry.list() == [(nick, whatid)]
 
 
-def test_regnick_all_nicknames():
-    # listing nicknames, reset
-    What.register_nickname('one', 'oneblah')
-    What.register_nickname('two', 'twoblah')
-    assert What.all_nicknames() == [('one', 'oneblah'), ('two', 'twoblah')]
-    What.reset_nicknames()
-    assert What.all_nicknames() == []
+def test_remove(registry, what2nick):
+
+    what, whatid, nick = what2nick
+
+    # remove by nickname
+    assert what == registry.register(what, nick)
+    assert registry.list() == [(nick, whatid)]
+    registry.remove(nickname=nick)
+    assert registry.list() == []
+
+    # remove by whatid
+    assert what == registry.register(what, nick)
+    assert registry.list() == [(nick, whatid)]
+    registry.remove(what=what)
+    assert registry.list() == []
+
+    # remove by both need to match the entry
+    assert what == registry.register(what, nick)
+    assert registry.list() == [(nick, whatid)]
+    with pytest.raises(ValueError):
+        registry.remove(what=what, nickname='NO')
+    assert registry.list() == [(nick, whatid)]
+    with pytest.raises(ValueError):
+        registry.remove(what='NO()', nickname=nick)
+
+    # remove does not accept None parameters
+    with pytest.raises(ValueError) as excinfo:
+        registry.remove()
+    assert 'at least one of nickname or what must be provided' == str(excinfo.value)
+
+
+def test_reset(registry, what2nick):
+    what, whatid, nick = what2nick
+    assert what == registry.register(what, nick)
+    assert registry.list() == [(nick, whatid)]
+    registry.reset()
+    assert registry.list() == []
