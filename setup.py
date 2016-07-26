7f0cee1d77ae3ce76d6402ca3d8e6c153fc16c5e
santi <sdvillal@gmail.com>
2016-07-26 13:00:33 +0200

    require toolz (just for curry)

--- setup.py
+++ setup.py
@@ -35,14 +35,13 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    install_requires=['arpeggio>=1.5', 'future>=0.15.2'],
+    install_requires=['arpeggio>=1.5', 'future>=0.15.2', 'toolz'],
     tests_require=['pytest', 'pytest-cov', 'pytest-pep8'],
     extras_require={
         'numpy': ['numpy'],
         'pandas': ['pandas'],
         'sklearn': ['scikit-learn'],
         'docs': ['Sphinx'],
-        'toolz': ['toolz'],
     },
     platforms=['Any'],
 )
