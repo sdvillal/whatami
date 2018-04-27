09c2c3a6fc4b706256a384d1addd03dc912d3bfd
santi <sdvillal@gmail.com>
2018-04-27 10:35:13 +0200

    allow id hashing for non-ascii strings in python 2
    waiting to fully ditch py2

--- whatami/what.py
+++ whatami/what.py
@@ -292,7 +292,10 @@ class What(object):
     def _trim_too_long(string, maxlength=0):
         """Returns the string or its sha1 if the string length is larger than maxlength."""
         if 0 < maxlength < len(string):
-            return hashlib.sha1(string.encode('utf-8')).hexdigest()
+            try:
+                return hashlib.sha1(string.encode('utf-8')).hexdigest()
+            except UnicodeError:
+                return hashlib.sha1(string.decode('utf-8').encode('utf-8')).hexdigest()
         return string
 
     # --- ID to dictionary
